028761918730fe12400ad2071c00f1ca0842d034
Erick Valencia <eredvago@live.com.mx>
2020-08-28 04:13:28 -0500

    deploy

---
+++ precache-manifest.7df0f1a06822f6168b544b77941ded39.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c282d92af5da7380537",
+    "url": "/wtools/css/app.bd70d789.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/wtools/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "102ae77e2e5ce84f8b599ce217e999f7",
+    "url": "/wtools/index.html"
+  },
+  {
+    "revision": "0d2e51d93a20d48b12c6",
+    "url": "/wtools/js/about.35b38420.js"
+  },
+  {
+    "revision": "4c282d92af5da7380537",
+    "url": "/wtools/js/app.1dcfc1be.js"
+  },
+  {
+    "revision": "7522c0720069a6f47515",
+    "url": "/wtools/js/chunk-vendors.ce061d87.js"
+  },
+  {
+    "revision": "6d2e8028b20f1f7d1ac0c06875f9ec6b",
+    "url": "/wtools/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/wtools/robots.txt"
+  }
+]);
